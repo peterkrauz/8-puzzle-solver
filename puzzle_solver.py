14583b610632eb41456788e6e99b29be422ef7d5
peterkrauz <krausepeterm@gmail.com>
2021-06-24 22:01:44 -0300

    feat: Add prompt for solution type

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -6,11 +6,21 @@ def solve_puzzle():
     puzzle_board = get_puzzle_board(puzzle_input)
     puzzle_board = assign_void_value_to_puzzle_board(puzzle_board)
 
-    print(puzzle_board)
+    solution_type = get_solution_type()
+
+    exhibit_puzzle_board(puzzle_board)
+
+
+def get_solution_type():
+    print('Deseja rodar o programa em qual dos seguintes modos?')
+    print('[1] Custo Uniforme')
+    print('[2] A* com heurística simples')
+    print('[3] A* com heurística precisa')
+    return int(input(''))
 
 
 def get_input_matrix():
-    input_file = open('input.txt', 'r')
+    input_file = open('initial_board.txt', 'r')
     puzzle_input = input_file.read().split(',')
     input_file.close()
     shuffle(puzzle_input)
@@ -21,7 +31,7 @@ def get_puzzle_board(puzzle_input):
     matrix_size = 3
     puzzle_board = [[] for _ in range(int(len(puzzle_input) / matrix_size))]
     for index, item in enumerate(puzzle_input):
-        puzzle_board[index % 3].append(item)
+        puzzle_board[index % 3].append(int(item.strip()))
 
     return puzzle_board
 
@@ -30,14 +40,28 @@ def assign_void_value_to_puzzle_board(puzzle_board):
     searching_for_void_square = True
     while searching_for_void_square:
 
-        print(puzzle_board)
-        void_square_value = input('Qual quadrado deseja pôr o valor vazio?')
-        user_choice = input(f'Deseja mesmo usar o quadrado {void_square_value}? (s/n)')
+        exhibit_puzzle_board(puzzle_board)
+        void_square_value = int(
+            input(
+                'Em qual quadrado deseja pôr o valor vazio? Obs.: insira o valor numérico,'
+                ' e não sua posição na matriz'
+            )
+        )
+        user_choice = input(f'Deseja mesmo usar o quadrado \033[94m{void_square_value}\033[0m? (s/n)\n')
 
         if user_choice == 's':
             row, column = find_index_for_value(puzzle_board, void_square_value)
             puzzle_board[row][column] = None
             return puzzle_board
+
+
+def exhibit_puzzle_board(puzzle_board):
+    for row in puzzle_board:
+        for item in row:
+            value_to_print = f'\033[94m{item}' if item else '\033[93m_'
+            print(f'{value_to_print} ', end='')
+        print()
+    print('\033[0m')
 
 
 def find_index_for_value(puzzle_board, value):
